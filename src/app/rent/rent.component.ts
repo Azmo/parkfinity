3811febe3121988472593923aa52bcf0394ef226
Alexander Zhidkov <alexander.zhidkov@empired.com>
2017-08-26 18:00:59 +0800

    ask user to log in when booking a bay

--- src/app/rent/rent.component.ts
+++ src/app/rent/rent.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { MdDialog } from '@angular/material';
+import { Router, RouterStateSnapshot } from '@angular/router';
+import { AngularFireAuth } from 'angularfire2/auth';
 import { AngularFireDatabase } from 'angularfire2/database';
 import * as moment from 'moment';
 import 'rxjs/add/operator/first';
 import { Observable } from 'rxjs/Observable';
+import { LoginComponent } from '../auth/login';
 
 @Component({
   selector: 'app-rent',
@@ -10,6 +14,7 @@ import { Observable } from 'rxjs/Observable';
   styleUrls: ['./rent.component.css'],
 })
 export class RentComponent implements OnInit {
+  routerStateSnapshot: RouterStateSnapshot;
   chosenDate: Date;
   bays: Observable<any>;
 
@@ -18,18 +23,29 @@ export class RentComponent implements OnInit {
     return moment(Date.now()).diff(d, 'days') < 1 && day !== 0 && day !== 6;
   }
 
-  constructor(private db: AngularFireDatabase) { }
+  constructor(private db: AngularFireDatabase, private router: Router, public dialog: MdDialog) { }
 
   ngOnInit() {
+    this.routerStateSnapshot = this.router.routerState.snapshot;
+  }
+
+  // ToDo refactor
+  isUserLoggedIn(): boolean {
+    localStorage.setItem('redirect', this.routerStateSnapshot.url);
+    if (!localStorage.getItem('currentUserId')) {
+      this.dialog.open(LoginComponent);
+    }
+    return true;
   }
 
   search() {
-    this.bays = this.db.list(`/bays/`, {
-      query: {
-        orderByChild: 'mon',
-        equalTo: true,
-      },
-    }).map((_bays) => _bays.filter((bay) => !bay.booked[moment(this.chosenDate).format('YYMMDD')]));
+    if (this.isUserLoggedIn()) {
+      this.bays = this.db.list(`/bays/`, {
+        query: {
+          orderByChild: 'mon',
+          equalTo: true,
+        },
+      }).map((_bays) => _bays.filter((bay) => !bay.booked[moment(this.chosenDate).format('YYMMDD')]));
+    }
   }
-
 }
