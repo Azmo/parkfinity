4d78fb734e13d9c815c060f1a02d939c705ec926
Alexander Zhidkov <alexander.zhidkov@empired.com>
2017-08-26 19:13:15 +0800

    wire up database for lease page

--- src/app/lease-my-bay/lease-my-bay.component.ts
+++ src/app/lease-my-bay/lease-my-bay.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase, FirebaseObjectObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-lease-my-bay',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lease-my-bay.component.css'],
 })
 export class LeaseMyBayComponent implements OnInit {
+  bay: FirebaseObjectObservable<any>;
 
-  constructor() { }
+  constructor(private db: AngularFireDatabase) { }
 
   ngOnInit() {
+    this.bay = this.db.object(`/bays/${localStorage.getItem('currentUserId')}`);
   }
 
 }
